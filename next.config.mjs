367b3b98c7feabbd349885116642f127a2981f59
dilawari2008 <dilawari2008@gmail.com>
2025-04-13 05:43:52 +0530

    Some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // Enables static export
+  basePath: isProd ? '/dilawari2008.github.io' : '', // Replace with your repository name (e.g., '/my-project')
+  assetPrefix: isProd ? '/dilawari2008.github.io/' : '', // Ensures assets are correctly pathed
+  images: {
+    unoptimized: true, // Disables Next.js image optimization (not supported on GitHub Pages)
+  },
 };
 
 export default nextConfig;
-
-
